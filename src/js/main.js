a748556ec773df166798d339e930cea41c26eadd
kknaguib <karimnaguib@Karims-MacBook.local>
2016-10-09 01:51:08 -0400

    Contact me section

--- src/js/main.js
+++ src/js/main.js
@@ -30,15 +30,23 @@ var map;
 
 function initMap() {
 
+  var torontoLatLng = {lat: 43.653226, lng: -79.38318429999998};
+
   // Create the map
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.86, lng: 151.209},
+    center: {lat: 43.653226, lng: -79.38318429999998},
     zoom: 13,
     mapTypeControl: false
   });
 
   // Set the map's style
-  map.setOptions({styles: styles["silver"]});
+  map.setOptions({styles: styles["night"]});
+
+  // Create a marker and set its position.
+  var marker = new google.maps.Marker({
+    map: map,
+    position: torontoLatLng
+  });
 }
 
 //Map Styles
@@ -130,6 +138,87 @@ var styles = {
       featureType: 'water',
       elementType: 'labels.text.fill',
       stylers: [{color: '#9e9e9e'}]
+    }
+  ],
+
+  night: [
+    {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+    {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+    {
+      featureType: 'administrative.locality',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+    {
+      featureType: 'poi',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'geometry',
+      stylers: [{color: '#263c3f'}]
+    },
+    {
+      featureType: 'poi.park',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#6b9a76'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry',
+      stylers: [{color: '#38414e'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#212a37'}]
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#9ca5b3'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry',
+      stylers: [{color: '#746855'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [{color: '#1f2835'}]
+    },
+    {
+      featureType: 'road.highway',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#f3d19c'}]
+    },
+    {
+      featureType: 'transit',
+      elementType: 'geometry',
+      stylers: [{color: '#2f3948'}]
+    },
+    {
+      featureType: 'transit.station',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#d59563'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'geometry',
+      stylers: [{color: '#17263c'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.fill',
+      stylers: [{color: '#515c6d'}]
+    },
+    {
+      featureType: 'water',
+      elementType: 'labels.text.stroke',
+      stylers: [{color: '#17263c'}]
     }
   ]
 };
